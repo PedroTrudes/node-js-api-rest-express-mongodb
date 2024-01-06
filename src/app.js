94a286599bb25149daa4a0010f87ac36e3c9b45d
PedroTrudes <pedro.jtrudes@gmail.com>
2024-01-06 16:38:09 -0300

    Crud completed

--- src/app.js
+++ src/app.js
@@ -14,17 +14,41 @@ const livros = [
     }
 ]
 
+function buscaLivro(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id)
+    });
+}
+
 app.get('/', (req, res) => {
     res.status(200).send("Chamando Rota principal");
 });
+
 
 app.get("/livros", (req, res) =>{
     res.status(200).json(livros)//usamos o metodo json pq estamos passando não somente uma string
 })
 
+app.get("/livros/:id", (req, res) => {
+    const indexLivro = buscaLivro(req.params.id);
+    res.status(200).json(livros[indexLivro])//buscando o indice no array
+})
+
 app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).send("Livro criado com sucesso");
+})
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros);
+})
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("livro removido com sucesso");
 })
 
 
