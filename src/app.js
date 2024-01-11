450581df2f6c930abc8541fa1dcda405c2dd67db
PedroTrudes <pedro.jtrudes@gmail.com>
2024-01-11 00:29:55 -0300

    created connection in mongo and found the first table  with find() and findById()

--- src/app.js
+++ src/app.js
@@ -1,37 +1,36 @@
 import express from 'express';
+import conectaNaDatabase from './config/dbConnect.js';
+import livro from './models/Livro.js';
+
+const conexao = await conectaNaDatabase();
+
+//se o evento acontecer na conexão esse erro chega como parametro na função
+conexao.on("error", (erro) => {
+    console.error("erro de conexão", erro);
+})
+
+conexao.once("open", () => {
+    console.log("Conexão com o banco feita com sucesso")
+})
 
 const app = express();
 app.use(express.json());// Middleware
 
-const livros = [
-    {
-        id: 1,
-        titulo: "O senhor dos aneis"
-    },
-    {
-        id: 2,
-        titulo: "O Hobbit"
-    }
-]
 
-function buscaLivro(id) {
-    return livros.findIndex(livro => {
-        return livro.id === Number(id)
-    });
-}
 
 app.get('/', (req, res) => {
     res.status(200).send("Chamando Rota principal");
 });
 
 
-app.get("/livros", (req, res) =>{
-    res.status(200).json(livros)//usamos o metodo json pq estamos passando não somente uma string
-})
+app.get("/livros", async (req, res) =>{
+    const listaLivros = await livro.find({});
+    res.status(200).json(listaLivros)//usamos o metodo json pq estamos passando não somente uma string
+}) 
 
-app.get("/livros/:id", (req, res) => {
-    const indexLivro = buscaLivro(req.params.id);
-    res.status(200).json(livros[indexLivro])//buscando o indice no array
+app.get("/livros/:id", async (req, res) => {
+    const listaLivrosById = await livro.findById(req.params.id);
+    res.status(200).json(listaLivrosById)//buscando o indice no array
 })
 
 app.post("/livros", (req, res) => {
